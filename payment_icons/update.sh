05d8efada69c0f97a361aedf55bc3b4b1a0d5c90
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-05-17 20:46:45 +0300

    Add Click & Pledge and Amazon icons

--- payment_icons/update.sh
+++ payment_icons/update.sh
@@ -9,9 +9,9 @@
 #
 
 echo "===> Paypal"
-wget http://paypal.com/favicon.ico -qO paypal.png # not really and ICO
+wget http://paypal.com/favicon.ico -qO paypal.png # not really an ICO
 
-echo "===> BitCoin"
+echo "===> Bitcoin"
 wget https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg -qO- | convert -size 32x32 -depth 8 -background transparent svg:- bitcoin.png
 
 echo "===> Flattr"
@@ -21,14 +21,19 @@ echo "===> Yandex.Money"
 wget https://money.yandex.ru/favicon.ico -qO- | convert 'ico:-[2]' yandex_money.png
 
 echo "===> Cheque"
-# cc-by
-wget http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/cheque-icon.png -qO cheque.png
+wget http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/cheque-icon.png -qO cheque.png # cc-by
 
 echo "===> WebMoney"
 wget http://www.wmtransfer.com/favicon.ico -qO- | convert 'ico:-[0]' webmoney.png
 
 echo "===> Western Union"
 wget https://www.westernunion.com/etc/designs/wu/favicon.ico -qO- | convert 'ico:-' western_union.png
+
+echo "===> Click & Pledge"
+wget https://clickandpledge.com/favicon.ico -qO- | convert 'ico:-' clickandpledge.png
+
+echo "===> Amazon"
+wget http://www.amazon.com/favicon.ico -qO- | convert 'ico:-[1]' amazon.png
 
 echo "===> Optimizing"
 optipng -q -o99 *.png
