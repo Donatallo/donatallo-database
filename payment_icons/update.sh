c2889935c4097860e3b3bb7219eba55000bbd8f8
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-08-29 18:26:40 +0300

    Improve updater script

--- payment_icons/update.sh
+++ payment_icons/update.sh
@@ -8,51 +8,61 @@
 # - optipng (for png optimization)
 #
 
-echo "===> Paypal"
-wget http://paypal.com/favicon.ico -qO paypal.png # not really an ICO
+for file in "$@"; do
+	echo "===> $file"
+	case $file in
+	paypal.png)
+		# not really an ICO
+		wget http://paypal.com/favicon.ico -qO $file
+		;;
+	bitcoin.png)
+		wget https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg -qO- | convert -size 32x32 -depth 8 -background transparent svg:- $file
+		;;
+	flattr.png)
+		wget https://flattr.com/images/favicons/favicon-32x32.png -qO $file
+		;;
+	yandex_money.png)
+		wget https://money.yandex.ru/favicon.ico -qO- | convert 'ico:-[2]' $file
+		;;
+	cheque.png)
+		wget http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/cheque-icon.png -qO $file
+		;;
+	webmoney.png)
+		# also may use https://www.webmoney.ru/img/wmkeeper_32x32.png, not sure which is more recognizable
+		wget https://www.webmoney.ru/img/icons/wmlogo_32.png -qO $file
+		;;
+#	western_union.png)
+#		# crap
+#		wget https://www.westernunion.com/etc/designs/wu/favicon.ico -qO- | convert 'ico:-' $file
+#		;;
+	clickandpledge.png)
+		# ugly 16x16 px crap
+		#wget https://clickandpledge.com/favicon.ico -qO- | convert 'ico:-' clickandpledge.png
+		wget https://forums.clickandpledge.com/core/avatars/0/0/1/7/4/5/avatar1_8.png -qO- | convert -channel rgba -resize 32x32 'png:-' $file
+		;;
+	amazon.png)
+		wget http://www.amazon.com/favicon.ico -qO- | convert 'ico:-[1]' $file
+		;;
+	patreon.png)
+		wget https://s3.amazonaws.com/patreon_public_assets/favicons/favicon-96x96.png -qO- | convert -channel rgba -resize 32x32 'png:-' $file
+		;;
+	litecoin.png)
+		wget https://litecoin.org/images/favicon/favicon-32x32-72a26c12.png -qO $file
+		;;
+	stripe.png)
+		wget https://stripe.com/favicon.ico -qO- | convert 'ico:-[1]' $file
+		;;
+	bountysource.png)
+		#wget https://www.bountysource.com/favicon.ico -qO- | convert 'ico:-' bountysource.png
+		wget https://cloudinary-a.akamaihd.net/bountysource/image/upload/d_noaoqqwxegvmulwus0un.png,c_pad,w_100,h_100,b_white/caxnpzz0uhmvxk9jfmz4.png -qO- | convert -channel rgba -resize 32x32 -depth 8 png:- $file
+		;;
+	dash.png)
+		wget https://www.dash.org/wp-content/uploads/2016/04/cropped-dash_icon_l-e1460606172897-1-32x32.png -qO $file
+		;;
+	*)
+		echo "$1 not supported, likely was crafted by hand"
+		;;
+	esac
 
-echo "===> Bitcoin"
-wget https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg -qO- | convert -size 32x32 -depth 8 -background transparent svg:- bitcoin.png
-
-echo "===> Flattr"
-wget https://flattr.com/images/favicons/favicon-32x32.png -qO flattr.png
-
-echo "===> Yandex.Money"
-wget https://money.yandex.ru/favicon.ico -qO- | convert 'ico:-[2]' yandex_money.png
-
-echo "===> Cheque"
-wget http://icons.iconarchive.com/icons/fatcow/farm-fresh/32/cheque-icon.png -qO cheque.png # cc-by
-
-echo "===> WebMoney"
-# also may use https://www.webmoney.ru/img/wmkeeper_32x32.png, not sure which is more recognizable
-wget https://www.webmoney.ru/img/icons/wmlogo_32.png -qO webmoney.png
-
-echo "===> Western Union"
-wget https://www.westernunion.com/etc/designs/wu/favicon.ico -qO- | convert 'ico:-' western_union.png
-
-echo "===> Click & Pledge"
-# ugly 16x16 px crap
-#wget https://clickandpledge.com/favicon.ico -qO- | convert 'ico:-' clickandpledge.png
-wget https://forums.clickandpledge.com/core/avatars/0/0/1/7/4/5/avatar1_8.png -qO- | convert -channel rgba -resize 32x32 'png:-' clickandpledge.png
-
-echo "===> Amazon"
-wget http://www.amazon.com/favicon.ico -qO- | convert 'ico:-[1]' amazon.png
-
-echo "===> Patreon"
-wget https://s3.amazonaws.com/patreon_public_assets/favicons/favicon-96x96.png -qO- | convert -channel rgba -resize 32x32 'png:-' patreon.png
-
-echo "===> Litecoin"
-wget https://litecoin.org/images/favicon/favicon-32x32-72a26c12.png -qO litecoin.png
-
-echo "===> Stripe"
-wget https://stripe.com/favicon.ico -qO- | convert 'ico:-[1]' stripe.png
-
-echo "===> Bountysource"
-#wget https://www.bountysource.com/favicon.ico -qO- | convert 'ico:-' bountysource.png
-wget https://cloudinary-a.akamaihd.net/bountysource/image/upload/d_noaoqqwxegvmulwus0un.png,c_pad,w_100,h_100,b_white/caxnpzz0uhmvxk9jfmz4.png -qO- | convert -channel rgba -resize 32x32 -depth 8 png:- bountysource.png
-
-echo "===> Dash"
-wget https://www.dash.org/wp-content/uploads/2016/04/cropped-dash_icon_l-e1460606172897-1-32x32.png -qO dash.png
-
-echo "===> Optimizing"
-optipng -q -o99 *.png
+	optipng -q -o99 $file
+done
